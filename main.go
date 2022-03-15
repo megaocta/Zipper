e081ffaf1382c597d1708006a208cadea0a27e81
Moritz <olli.twist@pm.me>
2022-03-15 19:09:37 +0100

    Directory listing works Delivering files works

--- main.go
+++ main.go
@@ -2,18 +2,76 @@ package main
 
 import (
 	"archive/zip"
+	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
+type Page struct {
+	Title                string
+	DirectoryListEntries []DirectoryListEntry
+}
+
+type DirectoryListEntry struct {
+	Icon string
+	Link string
+}
+
+var CurrentDir string
+var RootDir string
+
 func root(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Disposition", "attachment; filename=ComposedDownload.zip")
-	w.Header().Add("Content-Type", "application/zip")
-	archive := zip.NewWriter(w)
-	AddFileToZip("file.msi", archive)
-	AddFileToZip("file.pdf", archive)
-	archive.Close()
+	t := template.Must(template.ParseFiles("template.html"))
+	err := t.Execute(w, ListDirectory(CurrentDir))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// w.Header().Add("Content-Disposition", "attachment; filename=ComposedDownload.zip")
+	// w.Header().Add("Content-Type", "application/zip")
+	// archive := zip.NewWriter(w)
+	// AddFileToZip("file.msi", archive)
+	// AddFileToZip("file.pdf", archive)
+	// archive.Close()
+}
+
+func files(w http.ResponseWriter, r *http.Request) {
+	path := strings.Split(r.RequestURI, "/files/")
+	if len(path) > 1 {
+		f, err := os.Open(filepath.Join(CurrentDir, path[1]))
+		if err != nil {
+			w.Write([]byte("404 Not Found"))
+		}
+		io.Copy(w, f)
+	} else {
+		w.Write([]byte("404 Not Found"))
+	}
+}
+
+func ListDirectory(path string) Page {
+	entries, _ := os.ReadDir(CurrentDir)
+	var data []DirectoryListEntry
+	for _, entry := range entries {
+		if entry.IsDir() {
+			data = append(data, DirectoryListEntry{
+				Icon: "files/folder.png",
+				Link: entry.Name(),
+			})
+		} else {
+			data = append(data, DirectoryListEntry{
+				Icon: "files/file.png",
+				Link: entry.Name(),
+			})
+
+		}
+	}
+	return Page{
+		Title:                "Test",
+		DirectoryListEntries: data,
+	}
 }
 
 func AddFileToZip(filename string, archive *zip.Writer) {
@@ -23,7 +81,10 @@ func AddFileToZip(filename string, archive *zip.Writer) {
 }
 
 func main() {
-
+	CurrentDir, _ = os.Getwd()
+	log.Println("Current directory is " + CurrentDir)
+	RootDir = CurrentDir
 	http.HandleFunc("/", root)
+	http.HandleFunc("/files/", files)
 	http.ListenAndServe(":5000", nil)
 }
